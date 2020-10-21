4d8c38402ea8db97d7eec454a06975bf22d6dd3e
mio717 <72719562+mio717@users.noreply.github.com>
2020-10-22 00:51:16 +0900

    section2-q2_submit

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,21 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-    }
+      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)↓
+      for(let x = -5; x < 5; x++){
+        if(i == j+2*x+1){
+          fill('rgb(128,128,128)');
+          rect(size*i,size*j,size,size);
+          if(j < 3){
+            fill(255,0,0);
+            ellipse(size*(i+(1/2)),size*(j+(1/2)),size);
+          }
+          else if(j > 4){
+            fill(0,0,0);
+            ellipse(size*(i+(1/2)),size*(j+(1/2)),size);
+          }
+        }
+      }
+     }
+   }
   }
-}
