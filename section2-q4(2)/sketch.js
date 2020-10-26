e1f058bcf59f6074bfa9299e85e26238e9e83dbd
mio717 <72719562+mio717@users.noreply.github.com>
2020-10-26 09:46:36 +0900

    section2-q2(re)

--- section2-q4(2)/sketch.js
+++ section2-q4(2)/sketch.js
@@ -2,7 +2,7 @@
 function setup() {
   const red = color(255, 0, 0);
   const blue = color('rgb(0, 0, 102)');
-  createCanvas(190,100);
+  createCanvas(380,200);
   noStroke();
   background(255);
 
@@ -18,5 +18,14 @@ function setup() {
     rect(0, i * d, width, (i + 1) * d);
   }
   fill(blue);
-  rect(0, 0, 76, d * 7);
+  let p = width * 76 / 190
+  rect(0, 0, p, d * 7);
+
+  let size = p / 11;
+
+  for(let j = 0; j < 11; j++){
+    for(let k = 0; k < 9; k++){
+
+        }
+      }
 }
