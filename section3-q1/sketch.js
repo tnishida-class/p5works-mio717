0b034d0c684c3bf701df20db3f1e3741db226601
mio717 <72719562+mio717@users.noreply.github.com>
2020-11-10 14:04:20 +0900

    section3-q1
    練習問題：吹き出し

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -5,31 +5,30 @@ function setup(){
   createCanvas(400, 400);
   background(255);
   noStroke();
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46",100 ,0 ,204 ,120);
 }
 
-function balloon(t){
+function balloon(t, x, a, b, c){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  let green = color('rgb(0, 204,102)');
   let purple = color('rgb(210, 77, 255)');
   let q = w + p * 2;
   let r = h + p * 2;
   fill(purple);
-    ellipse(40, 44 + r / 2, r);
+    ellipse(x, (x + 4) + r / 2, r);
   fill(purple);
-    ellipse(38 + q, 44 + r / 2, r);
-  fill(green);
-   ellipse(40, 40 + r / 2, r);
-  fill(green);
-    ellipse(40 + q, 40 + r / 2, r);
-  fill(green);
-   rect(40, 40, q, r);
+    ellipse((x - 2) + q, (x + 4) + r / 2, r);
+  fill(a, b, c);
+   ellipse(x, x + r / 2, r);
+  fill(a, b, c);
+    ellipse(x + q, x + r / 2, r);
+  fill(a, b, c);
+   rect(x, x, q, r);
   fill(purple);
-   rect(40, 40 + r, q, p * 2);
+   rect(x, x + r, q, p * 2);
   fill(255);
-   text(t, p + 40, h + 40)
+   text(t, p + x, h + x)
   fill(purple);
-   triangle(135, 40 + r, 140, 60 + r, 145, 40 + r);
+   triangle((x + q - 15), x + r, (x + q - 10), (x + 20) + r, (x + q - 5), x + r);
 }
