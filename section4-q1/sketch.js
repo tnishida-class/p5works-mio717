197f65b0bb7d932069b537bd22562a1c4ffd707e
mio717 <72719562+mio717@users.noreply.github.com>
2020-11-12 13:30:00 +0900

    section4-q1
    心臓の鼓動のように大きくなったり小さくなったりする円

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,16 +1,23 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
 
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(windowWidth, windowHeight);
   count = 0;
   cycle = 100;
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
+  count = (count + 0.8) % cycle;
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  if(count < 50){
+    size = count;
+  }
+  else{
+    size = 100 - count;
+  }
+  ellipse(width / 2, height / 2, size * 5);
 }
