bf20f61a1531edfdf6c172841796a402bf823c88
mio717 <72719562+mio717@users.noreply.github.com>
2020-11-16 18:25:54 +0900

    section4-q2
    くるくる回転しながら流れる星

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,17 +1,25 @@
 // テキスト「アニメーション」
+//くるくる回転しながら流れる星
 let x, y, vx, vy;
+let angle = 0;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  x = width / 2;
-  y = height / 2;
-  vx = 8;
-  vy = 8;
+  x = width / 4;
+  y = height / 5;
+  vx = 5;
+  vy = 5;
 }
 
 function draw(){
-  background(160, 192, 255);
+  background(0, 51, 153);
   // BLANK[2] (hint: 作った star 関数を使います)
+  angle += 0.1;
+  noStroke();
+  fill(255, 255, 102);
+  star(x, y, 40, angle);
+  x += vx;
+  y += vy;
 
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
@@ -25,6 +33,7 @@ function star(cx, cy, r, angle){
   for(var i = 0; i < 20; i++){
     var theta = TWO_PI * i * 2 / 5 - HALF_PI;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
+    theta = theta + angle;
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
